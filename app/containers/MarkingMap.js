71b361f874a335ad8536ec1fab36351e507e1edd
Ryuichi Akiba <matsu6bei@gmail.com>
2016-12-28 13:16:42 +0900

    MapViewの導入。

--- app/containers/MarkingMap.js
+++ app/containers/MarkingMap.js
@@ -3,14 +3,25 @@ import {StyleSheet, Text, View} from "react-native";
 import {Actions} from 'react-native-router-flux';
 import Styles from '../themes/Styles';
 // import {connect} from "react-redux";
+import MapView from 'react-native-maps'
 
 class MarkingMap extends Component {
   render() {
     return (
+      // <View style={Styles.container}>
+      //   <Text style={Styles.instructions}>
+      //     MarkingMap Page!
+      //   </Text>
+      // </View>
       <View style={Styles.container}>
-        <Text style={Styles.instructions}>
-          MarkingMap Page!
-        </Text>
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </View>
     );
   }
