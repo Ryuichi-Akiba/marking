c2a00efee305a3080378bbaa4fb0e5f235273896
Ken Todoroki <ken.todoroki@majimena.co.jp>
2016-12-28 12:19:18 +0900

    reduxとreact-native-router-fluxをインテグレーション

--- app/App.js
+++ app/App.js
@@ -1,29 +1,46 @@
 import React from "react";
 import {Text} from "react-native";
-import {Scene, Router} from "react-native-router-flux";
+import {createStore, applyMiddleware, compose} from "redux";
+import {connect, Provider} from "react-redux";
+import {Scene, Router, Actions} from "react-native-router-flux";
+import reducers from "./reducers";
+import TabIcon from "./components/TabIcon"
 import Home from "./containers/Home";
 import MyPets from "./containers/MyPets";
 import MarkingMap from "./containers/MarkingMap";
 
-// 一時的に
-class TabIcon extends React.Component {
-  render(){
-    return (
-      <Text style={{color: this.props.selected ? "red" :"black"}}>{this.props.title}</Text>
-    );
-  }
-}
+// ReduxとRouterのインテグレーション
+const RouterWithRedux = connect()(Router);
+const middleware = [/* ...your middleware (i.e. thunk) */];
+const store = compose(applyMiddleware(...middleware))(createStore)(reducers);
 
-const App = () => (
-  <Router>
-    <Scene key="root">
-      <Scene key="Home" initial={true} hideNavBar={true} component={Home} title="HOME"/>
-      <Scene key="tabbar" tabs={true}>
-        <Scene key="MyPets" component={MyPets} title="PETS" icon={TabIcon}/>
-        <Scene key="MarkingMap" component={MarkingMap} title="MAP" icon={TabIcon}/>
-      </Scene>
+// シーンコンポーネントをReduxと接続したコンポーネントの定義
+const HomeComponent = connect()(Home);
+const MyPetsComponent = connect()(MyPets);
+const MarkingMapComponent = connect()(MarkingMap);
+
+// シーンを定義
+const scenes = Actions.create(
+  <Scene key="root">
+    <Scene key="home" initial={true} hideNavBar={true} component={HomeComponent}/>
+    <Scene key="tabbar" tabs={true}>
+      <Scene key="myPets" component={MyPetsComponent} title="PETS" icon={TabIcon}/>
+      <Scene key="markingMap" component={MarkingMapComponent} title="MAP" icon={TabIcon}/>
     </Scene>
-  </Router>
+  </Scene>
 );
 
-export default App;
+// アプリケーションのベース設定
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    return (
+      <Provider store={store}>
+        <RouterWithRedux scenes={scenes}/>
+      </Provider>
+    )
+  }
+};
