4f2198712f4a67a1f638bb2419a123e5ed45ba59
Ken Todoroki <ken.todoroki@majimena.co.jp>
2016-12-28 20:44:09 +0900

    何となくペット一覧できそうなところまで

--- app/AppRouter.js
+++ app/AppRouter.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {connect} from "react-redux";
 import {Scene, Router, Actions} from "react-native-router-flux";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {Container, Content, List, ListItem, Thumbnail, Text} from 'native-base';
 import Styles from "./themes/Styles";
-import TabIcon from "./components/TabIcon";
 import Home from "./containers/Home";
 import MyPets from "./containers/MyPets";
 import MarkingMap from "./containers/MarkingMap";
@@ -14,6 +15,22 @@ const RouterWithRedux = connect()(Router);
 const HomeComponent = connect()(Home);
 const MyPetsComponent = connect()(MyPets);
 const MarkingMapComponent = connect()(MarkingMap);
+
+// タブ定義
+class MyPetsTabIcon extends React.Component {
+  render(){
+    return (
+      <Icon name="paw" size={24} color={this.props.selected ? '#333333' : '#999999'} />
+    );
+  }
+}
+class MarkingMapTabIcon extends React.Component {
+  render(){
+    return (
+      <Icon name="map-o" size={24} color={this.props.selected ? '#333333' : '#999999'} />
+    );
+  }
+}
 
 // アプリケーションのシーン定義
 export default class AppRouter extends React.Component {
@@ -27,8 +44,8 @@ export default class AppRouter extends React.Component {
         <Scene key="root">
           <Scene key="home" initial={true} hideNavBar={true} component={HomeComponent}/>
           <Scene key="tabbar" tabs={true} tabBarStyle={Styles.tabBarStyle}>
-            <Scene key="myPets" component={MyPetsComponent} title="PETS" icon={TabIcon}/>
-            <Scene key="markingMap" component={MarkingMapComponent} title="MAP" icon={TabIcon}/>
+            <Scene key="myPets" component={MyPetsComponent} title="PETS" icon={MyPetsTabIcon}/>
+            <Scene key="markingMap" component={MarkingMapComponent} title="MAP" icon={MarkingMapTabIcon}/>
           </Scene>
         </Scene>
       </RouterWithRedux>
