7010686fa3b2e98cb755a10b09ba63998592e441
Ken Todoroki <ken.todoroki@majimena.co.jp>
2017-03-16 01:56:19 +0900

    Temp2

--- android/app/src/main/java/com/marking/MainApplication.java
+++ android/app/src/main/java/com/marking/MainApplication.java
@@ -3,10 +3,7 @@ package com.marking;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,10 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new MapsPackage(),
-            new PickerPackage(),
-            new FBSDKPackage()
+            new PickerPackage()
       );
     }
   };
