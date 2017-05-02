6355bc4c65b6115ba2b3ad9b23fcf347337d20b8
Ken Todoroki <ken.todoroki@majimena.co.jp>
2017-05-02 18:23:31 +0900

    開発環境を向くように設定ファイルを修正。

--- app/common/constants.js
+++ app/common/constants.js
@@ -1,10 +1,10 @@
 // TODO 環境依存設定なので、どこかに移動したい（動作環境に応じて設定を切り替えられるように）
 // 開発環境用定義
-// export const SERVER = 'http://petical-api-develop.ap-northeast-1.elasticbeanstalk.com';
-// export const CLIENT_ID = 'peticalconsole';
-// export const CLIENT_SECRET = 'p3dWz5lKF7WaFeP4lQRy';
+export const SERVER = 'http://petical-api-develop.ap-northeast-1.elasticbeanstalk.com';
+export const CLIENT_ID = 'peticalconsole';
+export const CLIENT_SECRET = 'p3dWz5lKF7WaFeP4lQRy';
 
 // ローカル環境用定義
-export const SERVER = 'http://localhost:8080';
-export const CLIENT_ID = 'majimenatestapp';
-export const CLIENT_SECRET = 'mySecretOAuthSecret';
+// export const SERVER = 'http://localhost:8080';
+// export const CLIENT_ID = 'majimenatestapp';
+// export const CLIENT_SECRET = 'mySecretOAuthSecret';
