85cb675b252cf70e10afd8a5c4705fa560681054
Ryuichi Akiba <matsu6bei@gmail.com>
2017-06-30 13:06:07 +0900

    FBSDKのzipファイル取得方法を修正。

--- provision/provisioning.sh
+++ provision/provisioning.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+FB_SDK_ZIPFILE=facebook-ios-sdk-current.zip
+
 if [ ! -d ~/Documents/FacebookSDK ]; then
   mkdir -p ~/Documents/FacebookSDK
 fi
 
-rm -f ./FacebookSDKs-iOS*.zip
+rm -f ./${FB_SDK_ZIPFILE}
 
-wget https://origincache.facebook.com/developers/resources/?id=facebook-ios-sdk-current.zip
+curl https://origincache.facebook.com/developers/resources/?id=facebook-ios-sdk-current.zip > ./${FB_SDK_ZIPFILE}
 
-unzip -u ./FacebookSDKs-iOS-*.zip -d ~/Documents/FacebookSDK
+unzip -u ./${FB_SDK_ZIPFILE} -d ~/Documents/FacebookSDK
 
 exit 0
