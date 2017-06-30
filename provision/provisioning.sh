7795fb381e36fce66b3dc72964c8e74eb906483c
Ryuichi Akiba <matsu6bei@gmail.com>
2017-06-30 15:46:16 +0900

    test

--- provision/provisioning.sh
+++ provision/provisioning.sh
@@ -2,6 +2,8 @@
 
 FB_SDK_ZIPFILE=facebook-ios-sdk-current.zip
 
+echo `pwd`
+
 if [ ! -d ~/Documents/FacebookSDK ]; then
   echo "Directory:~/Documents/FacebookSDK does not exists. make directory..."
   mkdir -p ~/Documents/FacebookSDK
@@ -13,7 +15,7 @@ fi
 #  curl https://origincache.facebook.com/developers/resources/?id=facebook-ios-sdk-current.zip > ./${FB_SDK_ZIPFILE}
 #  unzip -u ./${FB_SDK_ZIPFILE} -d ~/Documents/FacebookSDK
 #else
-  unzip -u ./FacebookSDKs-iOS-4.18.0.zip -d ~/Documents/FacebookSDK
+  unzip -u ./provision/FacebookSDKs-iOS-4.18.0.zip -d ~/Documents/FacebookSDK
 #fi
 
 exit 0
