a224435fe2b45fef9ae96189584ef1a005a81164
Ryuichi Akiba <matsu6bei@gmail.com>
2017-06-30 15:19:05 +0900

    FBSDKのバージョンは一旦固定でシェルを修正

--- provision/provisioning.sh
+++ provision/provisioning.sh
@@ -3,14 +3,17 @@
 FB_SDK_ZIPFILE=facebook-ios-sdk-current.zip
 
 if [ ! -d ~/Documents/FacebookSDK ]; then
+  echo "Directory:~/Documents/FacebookSDK does not exists. make directory..."
   mkdir -p ~/Documents/FacebookSDK
+else
+  echo "Directory:~/Documents/FacebookSDK already exists."
 fi
 
-if [ ! -e ./FacebookSDKs-iOS-*.zip  ]; then
-  curl https://origincache.facebook.com/developers/resources/?id=facebook-ios-sdk-current.zip > ./${FB_SDK_ZIPFILE}
-  unzip -u ./${FB_SDK_ZIPFILE} -d ~/Documents/FacebookSDK
-else
-  unzip -u ./FacebookSDKs-iOS-*.zip -d ~/Documents/FacebookSDK
-fi
+#if [ ! -e ./FacebookSDKs-iOS-*.zip  ]; then
+#  curl https://origincache.facebook.com/developers/resources/?id=facebook-ios-sdk-current.zip > ./${FB_SDK_ZIPFILE}
+#  unzip -u ./${FB_SDK_ZIPFILE} -d ~/Documents/FacebookSDK
+#else
+  unzip -u ./FacebookSDKs-iOS-4.18.0.zip -d ~/Documents/FacebookSDK
+#fi
 
 exit 0
